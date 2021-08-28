fd97175bb3e11699d2a98574e75c3a2959121988
Will Wang <bwang@smsassist.com>
2021-08-28 00:28:10 -0700

    Maven

---
+++ web-starter/src/main/java/multitenantsaas/webstarter/WebStarterApplication.java
@@ -0,0 +1,13 @@
+package multitenantsaas.webstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebStarterApplication.class, args);
+	}
+
+}
---
+++ web-starter/src/test/java/multitenantsaas/webstarter/WebStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package multitenantsaas.webstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
